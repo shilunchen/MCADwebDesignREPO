3ad370c7c34df40eac7733e2cbf3d63aca0fdf55
Shilun Chen <92143587+shilunchen@users.noreply.github.com>
2023-04-18 05:27:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var slider_img = document.querySelector(".slider_img");
+var images = [
+  "Kirby-and-the-Forgotten-Land_TOP_Banner-min.jpg",
+  "Kirby30th_Banner.png",
+  "Kirby30thfest_Banner.jpg",
+  "KirbyDreamBuffet_TOP_Banner-1.jpg",
+  "KirbyWiiDeluxe_TOP_Banner.png",
+  "ufo_TOP_Banner.jpg",
+];
+var i = 0;
+
+function prev() {
+  if (i <= 0) i = images.length;
+  i--;
+  return setImg();
+}
+
+function next() {
+  if (i >= images.length - 1) i = -1;
+  i++;
+  return setImg();
+}
+
+function setImg() {
+  return slider_img.setAttribute("src", "media/" + images[i]);
+}
