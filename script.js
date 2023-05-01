de821b29a3fae41a21adc972bcf485607af926be
Shilun Chen <92143587+shilunchen@users.noreply.github.com>
2023-05-01 07:14:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-var slider_img = document.querySelector(".slider_img");
-var images = [
-  "Kirby-and-the-Forgotten-Land_TOP_Banner-min.jpg",
-  "Kirby30th_Banner.png",
-  "Kirby30thfest_Banner.jpg",
-  "KirbyDreamBuffet_TOP_Banner-1.jpg",
-  "KirbyWiiDeluxe_TOP_Banner.png",
-  "ufo_TOP_Banner.jpg",
-];
-var i = 0;
-
-function prev() {
-  if (i <= 0) i = images.length;
-  i--;
-  return setImg();
-}
-
-function next() {
-  if (i >= images.length - 1) i = -1;
-  i++;
-  return setImg();
-}
-
-function setImg() {
-  return slider_img.setAttribute("src", "media/" + images[i]);
-}
-
 let eggBTN = document.querySelector(".topNav_Navbar_eggBTN");
 eggBTN.addEventListener("click", (e) => {
   document.querySelector(".hidden").style.transform = "translateY(0)";
@@ -34,3 +7,25 @@ let closeBTN = document.querySelector(".hiddenNav_closeBTN");
 closeBTN.addEventListener("click", (e) => {
   document.querySelector(".hidden").style.transform = "translateY(-1000px)";
 });
+
+// $(function () {
+//   $(".mainSlider").slick({
+//     prevArrow: ".arrow_prev",
+//   });
+// });
+
+// $(function () {
+//   $(".mainSlider").slick({
+//     nextArrow: ".arrow_next",
+//   });
+// });
+
+// $(".carousel-content").slick({
+//   prevArrow:
+//     "<img class='arrowPrev sliderBTN control-c prev slick-prev' src='media/worksSlideArrow.png'>",
+//   nextArrow:
+//     "<img class='arrowNext sliderBTN control-c next slick-next' src='media/worksSlideArrow.png'>",
+// });
+
+// prevArrow: <button type="button" class="sliderBTN slick-prev">previous</button>,
+//     nextArrow: <button type="button" class="sliderBTN slick-next">next</button>,
---
+++ slickController.js
@@ -0,0 +1,13 @@
+$(".mainSlider").slick({
+  dots: true,
+  infinite: true,
+  centerMode: true,
+  slidesToShow: 3,
+  slidesToScroll: 5,
+  variableWidth: true,
+  autoplay: true,
+  dots: true,
+  arrows: true,
+  prevArrow: ".arrow_prev",
+  nextArrow: ".arrow_next",
+});
