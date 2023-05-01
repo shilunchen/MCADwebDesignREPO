52f1f8cdbcc35bf228d5e98b3dd2bea55bd882a0
Shilun Chen <92143587+shilunchen@users.noreply.github.com>
2023-05-01 15:23:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,25 +7,3 @@ let closeBTN = document.querySelector(".hiddenNav_closeBTN");
 closeBTN.addEventListener("click", (e) => {
   document.querySelector(".hidden").style.transform = "translateY(-1000px)";
 });
-
-// $(function () {
-//   $(".mainSlider").slick({
-//     prevArrow: ".arrow_prev",
-//   });
-// });
-
-// $(function () {
-//   $(".mainSlider").slick({
-//     nextArrow: ".arrow_next",
-//   });
-// });
-
-// $(".carousel-content").slick({
-//   prevArrow:
-//     "<img class='arrowPrev sliderBTN control-c prev slick-prev' src='media/worksSlideArrow.png'>",
-//   nextArrow:
-//     "<img class='arrowNext sliderBTN control-c next slick-next' src='media/worksSlideArrow.png'>",
-// });
-
-// prevArrow: <button type="button" class="sliderBTN slick-prev">previous</button>,
-//     nextArrow: <button type="button" class="sliderBTN slick-next">next</button>,
